bb2014d9517fe44e690f3e0afc723613d4f6199c
kipmevrouw <beckyvandermeulen@hotmail.com>
2024-04-24 22:36:50 +0200

    feat: added hamburgermenu

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,4 +1,5 @@
 import Footer from "../components/Home-Sub-Components/Footer/Footer";
+import HamburgerMenu from "../components/Home-Sub-Components/HamburgerMenu/HamburgerMenu";
 import Navigation from "../components/Home-Sub-Components/Navigation/Navigation";
 import Section_1 from "../components/Home-Sub-Components/Section_1/Section_1";
 import Section_2 from "../components/Home-Sub-Components/Section_2/Section_2";
@@ -7,6 +8,9 @@ const Homepage = () => {
     return (
         <>
             <Navigation
+                Logo="/images/Home-Sub-images/TechCampusLogo.png"
+            />
+            <HamburgerMenu
                 Logo="/images/Home-Sub-images/TechCampusLogo.png"
             />
             <Section_1/>
