713e212b775184e4fb870338da42db475dedfdc2
kipmevrouw <beckyvandermeulen@hotmail.com>
2024-05-14 17:10:28 +0200

    started database connection

--- frontend/src/components/Signup-Components/SignupForm/SignupForm.js
+++ frontend/src/components/Signup-Components/SignupForm/SignupForm.js
@@ -1,26 +1,46 @@
 import { Link } from "react-router-dom";
 import "./SignupForm.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const SignupForm = ({ Logo, SignupImages }) => {
-  const [formData, setFormData] = useState({
-    email: "",
-    leerlingnummer: "",
+  const [gebruiker, setGebruiker] = useState({
+    voornaam: "",
+    achternaam: "",
     school: "",
+    code: "",
     uitslag1: "",
     uitslag2: "",
     password: "",
     repeatPassword: "",
+    checkboxVinkje: false,
   });
+
+  useEffect(() => {
+    const fetchGebruiker = async () => {
+      try {
+        const response = await fetch('/api/gebruiker');
+        const data = await response.json();
+        setGebruiker(data);
+      } catch (error) {
+        console.error('Fout bij ophalen van gebruiker:', error);
+      }
+    };
+
+    fetchGebruiker();
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setGebruiker({ ...gebruiker, [name]: value });
   };
 
   const handleSubmit = (e) => {
+    if (!gebruiker.code.startsWith("tyt")) {
+      alert("Ongeldige code. Vraag jouw code aan de docent!");
+      return;
+    }
     e.preventDefault();
-    if (formData.password !== formData.repeatPassword) {
+    if (gebruiker.password !== gebruiker.repeatPassword) {
       alert("Wachtwoorden komen niet overeen");
       return;
     }
@@ -45,22 +65,24 @@ const SignupForm = ({ Logo, SignupImages }) => {
           <form onSubmit={handleSubmit} className="form">
             <div className="SignupForm_form_input">
               <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
+                type="text"
+                name="voornaam"
+                placeholder="Voornaam"
+                value={gebruiker.voornaam}
                 onChange={handleChange}
                 className="SignupForm_input"
+                required
               />
             </div>
             <div className="SignupForm_form_input">
               <input
                 type="text"
-                name="leerlingnummer"
-                placeholder="Leerlingnummer"
-                value={formData.leerlingnummer}
+                name="achternaam"
+                placeholder="Achternaam"
+                value={gebruiker.achternaam}
                 onChange={handleChange}
                 className="SignupForm_input"
+                required
               />
             </div>
             <div className="SignupForm_form_input">
@@ -68,23 +90,38 @@ const SignupForm = ({ Logo, SignupImages }) => {
                 type="text"
                 name="school"
                 placeholder="School"
-                value={formData.school}
+                value={gebruiker.school}
                 onChange={handleChange}
                 className="SignupForm_input"
+                required
+              />
+            </div>
+            <div className="SignupForm_form_input">
+              <input
+                type="text"
+                name="code"
+                placeholder="Code (Ontvangen van je docent)"
+                value={gebruiker.code}
+                onChange={handleChange}
+                className="SignupForm_input"
+                required
               />
             </div>
             <div className="SignupForm_form_input">
               <select
                 name="uitslag1"
-                value={formData.uitslag1}
+                value={gebruiker.uitslag1}
                 onChange={handleChange}
                 className="SignupForm_input"
+                required
               >
                 <option value="" className="SignupForm_input_dropdown">
                   - Selecteer jouw uitslag -
                 </option>
                 <option value="De-vernieuwer">De vernieuwer</option>
-                <option value="De-maatschappelijke-toepasser">De maatschappelijke toepasser</option>
+                <option value="De-maatschappelijke-toepasser">
+                  De maatschappelijke toepasser
+                </option>
                 <option value="De-ontdekker">De ontdekker</option>
                 <option value="De-creatieve-maker">De creatieve maker</option>
                 <option value="De-doener">De doener</option>
@@ -93,15 +130,18 @@ const SignupForm = ({ Logo, SignupImages }) => {
             <div className="SignupForm_form_input">
               <select
                 name="uitslag2"
-                value={formData.uitslag2}
+                value={gebruiker.uitslag2}
                 onChange={handleChange}
                 className="SignupForm_input"
+                required
               >
                 <option value="" className="SignupForm_input_dropdown">
                   - Selecteer jouw uitslag -
                 </option>
                 <option value="De-vernieuwer">De vernieuwer</option>
-                <option value="De-maatschappelijke-toepasser">De maatschappelijke toepasser</option>
+                <option value="De-maatschappelijke-toepasser">
+                  De maatschappelijke toepasser
+                </option>
                 <option value="De-ontdekker">De ontdekker</option>
                 <option value="De-creatieve-maker">De creatieve maker</option>
                 <option value="De-doener">De doener</option>
@@ -112,7 +152,7 @@ const SignupForm = ({ Logo, SignupImages }) => {
                 type="password"
                 name="password"
                 placeholder="Wachtwoord"
-                value={formData.password}
+                value={gebruiker.password}
                 onChange={handleChange}
                 className="SignupForm_input"
               />
@@ -122,10 +162,24 @@ const SignupForm = ({ Logo, SignupImages }) => {
                 type="password"
                 name="repeatPassword"
                 placeholder="Herhaal wachtwoord"
-                value={formData.repeatPassword}
+                value={gebruiker.repeatPassword}
                 onChange={handleChange}
                 className="SignupForm_input"
               />
+            </div>
+            <div className="SignupForm_form_vinkje">
+              <input
+                type="checkbox"
+                id="agreement"
+                name="checkbox"
+                value={gebruiker.checkboxVinkje}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="agreement" className="SignupForm_form_agreetext">
+                Ik ga akkoord met de{" "}
+                <a href="/voorwaarden">algemene voorwaarden</a>.
+              </label>
             </div>
             <button type="submit" className="SignupForm_Button">
               Registreren
